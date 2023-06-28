df5761af3b436a12ee16fc037d64c99b41f88862
olaniyitomilola <54294935+olaniyitomilola@users.noreply.github.com>
2023-06-28 09:17:54 +0100

    calendar implemented

--- src/DisplayPort.jsx
+++ src/DisplayPort.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import Home from "./Home";
+import EventBooking from "./EventBooking";
+import Orders from "./Orders";
+import Inventory from "./Inventory";
+
 
 function DisplayPort({activeNav}) {
 
   return (
     <div className="displayPort">
 
-      {activeNav === "Home"? <Home/> : ""}
+      {activeNav === "Home"? <Home/> : activeNav === "Event Bookings" ? <EventBooking/> : activeNav === "Orders"? <Orders/> : <Inventory/>}
     </div>
   );
 }
---
+++ src/EventBooking.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import Calender from 'react-calendar'
+
+
+export default function EventBooking(){
+    const [value, onChangeValue] = useState(new Date())
+    
+    const events = [
+        {"date" : "28-June-2023", "event" : "Birthday Party at Duxford", "Hours": "7Hrs"},
+        {"date" : "29-June-2023", "event" : "Birthday Party at Cambridge", "Hours": "10Hrs"},
+        {"date" : "29-June-2023", "event" : "Graduation Party at Langley", "Hours": "3Hrs"}
+
+
+    ]
+    return(
+            <>
+                   <Calender showNeighboringMonth = {false} onChange={onChangeValue} value={value}/>
+                    <EventsLoader events = {events}/>
+            </>
+         
+            )
+}
+
+
+const EventsLoader = (props)=>{
+    return(
+           <table className='eventTable'>
+                <tr className='tableHead'>
+                    <th>Date</th>
+                    <th className='eventsDesc'>Event</th>
+                    <th>Booking Hours</th>
+                </tr>
+                {props.events.map((event)=>
+                    <tr>
+                        <td>{event.date}</td>
+                        <td>{event.event}</td>
+                        <td>{event.Hours}</td>
+                    </tr>
+
+            
+                    
+                )}
+
+            </table>
+
+    )
+}
