796c8f31495a6c462bd20c2b2cecb060a75cb561
olaniyitomilola <54294935+olaniyitomilola@users.noreply.github.com>
2023-08-03 20:10:59 +0100

    controller added

--- src/Inventory.jsx
+++ src/Inventory.jsx
@@ -1,32 +1,75 @@
+import { useEffect, useState } from "react"
+
 export default function Inventory(){
 
-    const inventory = [
-        {name: "Amala", price: 5, unit: "Scoop", img: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Amala.jpg/1024px-Amala.jpg"}
-        ,{name: "White Rice", price: 2.80, unit: "Spoon", img: "https://static01.nyt.com/images/2018/02/21/dining/00RICEGUIDE8/00RICEGUIDE8-superJumbo.jpg"}
-        ,{name: "Nigerian Jollof Rice", price: 3, unit: "Spoon", img: "https://i.etsystatic.com/43736205/r/il/7a1cd7/5083482431/il_794xN.5083482431_aj9p.jpg"}
-        ,{name: "Stir Fried Rice", price: 3.5, unit: "Spoon", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA6F4LyczKxY_dkTpu6byAPDuxJqPHrWq4Zw&usqp=CAU"}
+    const [inventory,setInventory] = useState([]);
+
+    
+    useEffect( ()=>{
+        async function fetchData(){
+        const items = await  fetch('http://localhost:3002');
+        let res = await items.json();
+        return res
+        
+    }
+    fetchData()
+        .catch(console.error)
+        .then((res)=> {setInventory(res)})
+        
+    },[])
 
 
-    ]
+ 
 
 
     return (
         <div className="inventory">
-            <InventoryLoader inventory={inventory}/>
+            {!inventory.length? "Loading" : <InventoryLoader setInventory={setInventory} inventory={inventory}/>}
+            
         </div>
     )
 }
 function InventoryLoader(props){
+
+    const handleDelete = async (key) =>{
+        try{
+            let req = await fetch(`http://localhost:3002/${key}`,{
+            method: 'delete'
+            })
+            let res = await req.json()
+            if(res.items){
+                props.setInventory(res.items)
+            }
+
+        }catch(error){
+            console.error(error)
+        }
+        
+    } 
+    
     return (
-        props.inventory.map((item)=>
-            <div className="inventorybox">
-                <img className="inventoryPicture" src={item.img} alt={"Picture of " +item.name} srcset="" />
-                <div className="inventoryDetails">
-                    <div className="inventoryName">{item.name}</div>
-                    <div  className="inventoryPrice">{"£" + item.price + " per " + item.unit}</div>
+        props.inventory.map((item)=>{
+            //The if is here because we are currently serving array from
+            //the server and want to set item to null when we delete
+            //to remove this when we serve from db.
+            {if(item){
+                return(
+                <div key={props.inventory.indexOf(item)} className="inventorybox">
+                    <img className="inventoryPicture" src={item.img} alt={"Picture of " +item.name} srcset="" />
+                    <div className="inventoryDetails">
+                        <div className="inventoryName">{item.name}</div>
+                        <div  className="inventoryPrice">{"£" + item.price + " per " + item.unit}</div>
+                    </div>
+                    <div className="buttons">
+                        <button className="inventoryEdit">Edit</button>
+                        <button className="inventoryEdit" onClick={async ()=> await handleDelete(props.inventory.indexOf(item))}>Delete</button>
+
+
+                    </div>
                 </div>
-                <button className="inventoryEdit">Edit</button>
-            </div>
-        )
+                )
+            }}
+            
+        })
     )
 }
